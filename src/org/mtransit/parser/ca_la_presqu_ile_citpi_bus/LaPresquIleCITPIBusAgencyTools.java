0e62d444ba6a81d27fedd01d91e2f80779bd955a
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:03:09 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_la_presqu_ile_citpi_bus/LaPresquIleCITPIBusAgencyTools.java
+++ src/org/mtransit/parser/ca_la_presqu_ile_citpi_bus/LaPresquIleCITPIBusAgencyTools.java
@@ -35,7 +35,7 @@ public class LaPresquIleCITPIBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating CITPI bus data...\n");
+		System.out.printf("\nGenerating CITPI bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
